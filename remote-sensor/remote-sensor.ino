cfe1c6e186bfe087048f024506d0c45104c4b857
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2020-11-25 01:25:45 -0800

    remove use of String and use char* instead. simplify id selection (use hard coded constant for now).

--- remote-sensor/remote-sensor.ino
+++ remote-sensor/remote-sensor.ino
@@ -1,7 +1,6 @@
 #include <RH_ASK.h>
 
 #define LM35_LOAD_PIN A7
-#define LM35_UNITS LM35_FAHRENHEIT
 
 #include <LM35.h>
 
@@ -12,22 +11,17 @@
 #define RF_PIN_TX 12
 #define RF_PIN_PTT 255
 
-#define DIP1 2
-#define DIP2 3
-
 #define DEBUG
 
 static RH_ASK rf;
 static LM35 temp;
 
-static String id;
+static uint8_t id = 0; // TODO make configurable
 
 void setup() {
     analogReference(DEFAULT);
     Serial.begin(9600);
     Serial.println("started setup");
-    pinMode(DIP1, INPUT);
-    pinMode(DIP2, INPUT);
     if (!rf.init()) {
 #ifdef DEBUG
     Serial.println(F("init RF module failed"));
@@ -35,24 +29,8 @@ void setup() {
     } else {
         rf.setModeTx();
     }
-    // testdrawstyles();
-    Debounced d1 = Debounced(DIP1);
-    Debounced d2 = Debounced(DIP2);
 
     delay(2000);
-    bool d1change = false;
-    bool d2change = false;
-    int start = millis();
-    do {
-        d1change = d1.read();
-        d2change = d2.read();
-    } while ((!d1change && !d2change) || (millis() - start) > 10000);
-    Serial.println(d1.value());
-    Serial.println(d2.value());
-    byte tmpid = d1.value() + (d2.value() << 1);
-    String hexId = String((tmpid & 0x00F0) >> 4, HEX);
-    hexId = hexId + String((tmpid & 0x000F), HEX);
-    id = hexId;
 #ifdef DEBUG 
     Serial.print(F("Started id="));
     Serial.println(id);
@@ -66,16 +44,17 @@ void loop() {
 
     double t = temp.tempAsF();
     Serial.println(t,3);
-    String stemp = String(t, 3);
-    String msg = id + stemp + "\0";
+    char *msg = (char*)malloc(10*sizeof(char));
+    char *cstemp = (char*)malloc(8*sizeof(char));
+    uint8_t mag = (uint8_t)log10(abs(t));
+    Serial.println(mag);
+    dtostrf(t, 3 + mag, 1, cstemp);
+    Serial.println(cstemp);
+    snprintf(msg, 9, "%02d%s", id, cstemp);
+    free(cstemp);
+    
 
-#ifdef DEBUG
-    Serial.println(millis());
-//     Serial.println(stemp);
-//     Serial.println(msg);
-#endif
-
-    rf.send((uint8_t*)msg.c_str(), msg.length());
+    rf.send((uint8_t*)msg, strlen(msg));
     rf.waitPacketSent();
 #ifdef DEBUG
     Serial.print(millis());
@@ -85,25 +64,6 @@ void loop() {
     Serial.print(msg);
     Serial.println("\"");
 #endif
-    delay(1500);
+    free(msg);
+    delay(2500);
 }
-
-// void testdrawstyles(void) {
-//   display.clearDisplay();
-
-//   display.setTextSize(1);             // Normal 1:1 pixel scale
-//   display.setTextColor(SSD1306_WHITE);        // Draw white text
-//   display.setCursor(0,0);             // Start at top-left corner
-//   display.println(F("Hello, world!"));
-
-//   display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-//   display.println(3.141592);
-
-//   display.setTextSize(2);             // Draw 2X-scale text
-//   display.setTextColor(SSD1306_WHITE);
-//   display.print(F("0x"));
-//   display.println(0xDEADBEEF, HEX);
-
-//   display.display();
-//   delay(2000);
-// }
