b5ec5ec884045170c1b8164f4f25c3d9d77142a0
littleaj <1690572+littleaj@users.noreply.github.com>
2020-11-25 01:27:35 -0800

    remove use of String and use char*. start to refactor display into separate class. provided a better time-keeping mechanism.

--- temp-alarm-base/temp-alarm-base.h
+++ temp-alarm-base/temp-alarm-base.h
@@ -1,7 +1,48 @@
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiAvrI2c.h>
+
+#include <MemoryFree.h>
+
 #define NULL_TEMP_VALUE -999
+#define TEMP_TIMEOUT 45000
 
 struct RemoteTemp {
-    int id = -1;
-    long lastMessageTime = 0;
     double value = NULL_TEMP_VALUE;
+    long lastMessageTime = 0;
+    int id = NULL_TEMP_VALUE;
+    char label[7]; // label[6] == '\0'
 };
+
+#define MAX_NUMBER_OF_TEMP_SENSORS 8
+
+#define _ALPHANUMERIC_FONT X11fixed7x14B
+#define _UNICODE_FONT utf8font10x16
+
+class TempDisplayQueue {
+    public:
+        TempDisplayQueue();
+        ~TempDisplayQueue();
+        bool begin(RemoteTemp* buffer = NULL);
+        RemoteTemp* temps;
+    private:
+};
+
+TempDisplayQueue::TempDisplayQueue() {
+}
+
+TempDisplayQueue::~TempDisplayQueue() {
+    free(temps);
+}
+
+bool TempDisplayQueue::begin(RemoteTemp* buffer) {
+    if (buffer != NULL) {
+        temps = buffer;
+    }
+    if (temps == NULL) {
+        return false;
+    }
+    for (int i = 0; i < 8; i++) {
+        RemoteTemp t;
+        temps[i] = t;
+    }
+}
--- temp-alarm-base/temp-alarm-base.ino
+++ temp-alarm-base/temp-alarm-base.ino
@@ -1,4 +1,4 @@
-#include <SSD1306Ascii.h>
+// #include <SSD1306Ascii.h>
 #include <SSD1306AsciiAvrI2c.h>
 
 // #include <LiquidCrystal.h>
@@ -8,20 +8,14 @@
 #include "temp-alarm-base.h"
 
 #define NUMBER_OF_SAMPLES 128
-#define SAMPLING_DELAY 15
-
-#define LCD_PIN_RS 12
-#define LCD_PIN_E 11
-#define LCD_PIN_RW 10
-#define LCD_PIN_D0 2
-#define LCD_PIN_D1 3
-#define LCD_PIN_D2 4
-#define LCD_PIN_D3 5
+#define SAMPLING_DELAY 50
 
 #define RF_BITRATE 2000
 #define RF_PIN_RX 11
 #define RF_PIN_TX 255
 #define RF_PIN_PTT 255
+
+#define DISPLAY_PERIOD 6000
 
 #define DEBUG
 
@@ -30,12 +24,13 @@ static SSD1306AsciiAvrI2c oled;
 
 #define I2C_ADDRESS 0X3C
 
-#ifdef DEBUG
-static uint16_t lastTime = 0L;
-#endif
-
 static LM35 temp;
-static uint8_t labelWidth;
+static RemoteTemp temps[8];
+static int8_t displayIndex = -1;
+static uint8_t remoteCount = 0;
+static unsigned long lastDisplayTime = 0;
+static const unsigned long startTime = millis();
+static unsigned long elapsed = 0;
 
 void setup() {
   analogReference(INTERNAL);
@@ -54,113 +49,191 @@ void setup() {
 #endif
   }
 
-  Serial.println("starting display");
+  Serial.println(F("starting display"));
   oled.begin(&Adafruit128x64, I2C_ADDRESS);
-  oled.setFont(font8x8);
+  oled.setFont(Callibri15);
   oled.clear();
-  oled.println();
-  uint8_t currentWidth = 0;
-  const char* label = "Base:";
-  oled.print(label);
-  labelWidth = oled.strWidth(label);
-  currentWidth += labelWidth;
-  oled.setFont(Adafruit5x7);
   oled.set2X();
-  const char* blankTemp = "    ";
-  currentWidth += oled.strWidth(blankTemp);
-  oled.print(blankTemp);
-  oled.setFont(font8x8);
-  oled.set1X();
-  oled.setCursor(currentWidth+2, 0);
-  oled.print("o");
-  oled.setFont(Adafruit5x7);
-  oled.set2X();
-  oled.println("F");
+  // TODO would be cool in a ticker
+  oled.setFont(Callibri11);
+  oled.println(F("Temperature"));
+  oled.setFont(Callibri15);
+  oled.println(F("Alarm"));
+  delay(3000);
+  oled.clear();
+  mem("setup_end");
 }
 
-
-
-#define TEMP_TIMEOUT 2500
+void mem(char* label) {
+  Serial.print(F("@"));
+  Serial.print(label);
+  Serial.print(F(" mem: "));
+  Serial.println(freeMemory());
+}
 
 RemoteTemp parseTemp(char* msg) {
   RemoteTemp t;
-  t.lastMessageTime = millis();
-  String smsg = String(msg);
-  t.id = (byte)smsg.substring(0,2).toInt();
-  t.value = smsg.substring(2).toDouble();
+  t.lastMessageTime = elapsed;
+  
+  char cid[3];
+  strncpy(cid, msg, 2);
+  cid[2] = '\0';
+  t.id = atoi(cid);
+
+  char cval[7];
+  memcpy(cval, &msg[2], 6);
+  cval[6] = '\0';
+  t.value = atof(cval);
   return t;
 }
 
 bool isTempExpired(RemoteTemp t) {
-  return millis() - t.lastMessageTime > TEMP_TIMEOUT;
+  return elapsed - t.lastMessageTime > TEMP_TIMEOUT;
 }
 
-static RemoteTemp temps[8];
 
 void loop() {
-  // int reading = analogRead(A0); // [0, 1024)
-  // sumSq += LM35_CONVERSION_CONSTANT*LM35_CONVERSION_CONSTANT * reading*reading;
-
-  uint8_t buf[10];
-  uint8_t buflen = sizeof(buf);
+  elapsed = millis() - startTime;
+  uint8_t *buf = (uint8_t*)malloc(10*sizeof(uint8_t)); 
+  uint8_t buflen = 10;
   if (rf.recv(buf, &buflen)) {
-    Serial.print("Received message: ");
+    Serial.print(elapsed);
+    Serial.print(F(": Received message: "));
+    buf[9] = '\0'; // just to be sure
     Serial.println((char*)buf);
     RemoteTemp tmsg = parseTemp((char*)buf);
-    if (tmsg.id >= 0) {
-      temps[tmsg.id] = tmsg;
+    if (tmsg.id >= 0 && tmsg.id <= 7) {
+      bool found = false;
+      for (int i = 0; i < remoteCount; i++) {
+        if (temps[i].id == tmsg.id) {
+          Serial.print("Found id = ");
+          Serial.println(tmsg.id);
+          temps[i] = tmsg;
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        Serial.print("New sensor. count=");
+        Serial.println(remoteCount);
+        temps[remoteCount++] = tmsg;
+      }
     }
   }
-
-#ifdef DEBUG
-  uint16_t thisTime = millis();
-  uint16_t elapsed = thisTime - lastTime;
-  if (elapsed > 2500) {
-    Serial.print(thisTime);
-    Serial.println(": waiting");
-    lastTime = thisTime;
-  }
-#endif
+  free(buf);
+  
+  double baseTemp = temp.tempAsF();
+  double sum = baseTemp;
   if (!temp.sampleTemp()) {
     return;
   }
-  // if (++sampleIndex < NUMBER_OF_SAMPLES) {
-  //   delay(SAMPLING_DELAY);
-  //   return;
-  // }
-  // sampleIndex = 0;
-  // double val = toF(sqrt(sumSq / NUMBER_OF_SAMPLES));
-  // sumSq = 0.0;
+  mem("after_sample");
+
 #ifdef DEBUG
  Serial.print(F("BASE temp: "));
- Serial.println(temp.tempAsF(), 3);
+ Serial.println(baseTemp, 3);
  Serial.println();
- 
-  oled.setCursor(labelWidth, 0);
-  oled.println(temp.tempAsF(), 1);
-
- for (int i = 0; i < 8; i++) {
-   RemoteTemp t = temps[i];
-   if (isTempExpired(t)) {
-     t.id = -1;
-   }
-   if (t.id >= 0) {
-     oled.setFont(font8x8);
-     oled.setCursor(0, 25);
-     oled.set1X();
-     oled.print("Sen");
-     oled.print(t.id);
-     oled.print(":");
-     oled.setCursor(labelWidth, 9);
-     oled.setFont(Adafruit5x7);
-     oled.set2X();
-     oled.println(t.value, 1);
-     Serial.print(F("Sensor "));
-     Serial.print(t.id);
-     Serial.print(F(": "));
-     Serial.println(t.value, 3);
-   }
- }
 #endif
+
+  for (int i = 0; i < 8; i++) {
+    RemoteTemp t = temps[i];
+    if (t.id <= NULL_TEMP_VALUE) {
+      continue;
+    }
+    sum += t.value;
+    if (isTempExpired(t)) {
+      remoteCount--;
+      displayIndex--;
+      RemoteTemp t2;
+      temps[i] = t2;
+      // shift values left
+      int lastEmpty = i;
+      for (int j = i + 1; j < 8; j++) {
+        if (temps[j].id > -999) {
+          RemoteTemp tmp = temps[lastEmpty];
+          temps[lastEmpty] = temps[j];
+          temps[j] = tmp;
+          lastEmpty++;
+        }
+      }
+      oled.clear();
+      mem("after_expire");
+    }
+  }
+
+  oled.home();
+  oled.setFont(_ALPHANUMERIC_FONT);
+  oled.set1X();
+  Serial.println();
+  if (remoteCount > 0) {
+    printAvgAndCycle(sum);
+  } else {
+    oled.print(F("Base: "));
+    oled.set2X();
+    oled.print(baseTemp, 1);
+    printUnitSuffix();
+  }
+
+
+
+#ifdef DEBUG
+  for (int i = 0; i < 8; i++) {
+    RemoteTemp t = temps[i];
+    if (t.id <= -999) continue;
+    Serial.print(F("Sensor "));
+    Serial.print(t.id);
+    Serial.print(F(": "));
+    Serial.println(t.value, 3);
+  }
+#endif
+
   delay(SAMPLING_DELAY);
+}
+
+void printAvgAndCycle(double sum) {
+  oled.set2X();
+  uint8_t rows = oled.fontRows();
+  oled.clear(0,128,0,rows);
+  oled.set1X();
+  oled.print(F("Avg:  "));
+  oled.set2X();
+  oled.print(sum / (remoteCount + 1), 1);
+  printUnitSuffix();
+
+  oled.setFont(_ALPHANUMERIC_FONT);
+  oled.set1X();
+  oled.clear(0,128,rows, rows+rows-1);
+  if (displayIndex == -1) {
+    oled.print("Base: ");
+    oled.set2X();
+    oled.print(temp.tempAsF(), 1);
+  } else {
+    RemoteTemp t = temps[displayIndex];
+    oled.print(F("Sen"));
+    oled.print(t.id);
+    oled.print(F(": "));
+    oled.set2X();
+    oled.print(t.value, 1);
+  }
+  printUnitSuffix();
+  unsigned long period = elapsed - lastDisplayTime;
+  bool triggerNext = period > DISPLAY_PERIOD;
+  Serial.print("period=");
+  Serial.println(period);
+  if (triggerNext) {
+    lastDisplayTime = elapsed;
+    if (++displayIndex >= remoteCount) {
+      displayIndex = -1;
+    }
+    Serial.print("displayIndex: ");
+    Serial.println(displayIndex);
+  }
+}
+
+
+void printUnitSuffix() {
+  oled.setFont(_UNICODE_FONT);
+  oled.print('\260');
+  oled.setFont(_ALPHANUMERIC_FONT);
+  oled.println('F');
 }
