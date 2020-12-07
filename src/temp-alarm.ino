bbbd223af3770101207572a48c0d2ede392d6f77
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2020-12-07 01:34:21 -0800

    first pass at serial keypad reading

--- src/temp-alarm.ino
+++ src/temp-alarm.ino
@@ -6,7 +6,7 @@
 #include <SSD1306AsciiAvrI2c.h>
 #include <EEPROM.h>
 
-#include <TempAlarmControl.h>
+// #include <TempAlarmControl.h>
 
 #define I2C_ADDRESS 0x3C
 #define RST_PIN -1
@@ -14,19 +14,19 @@
 #define KEYPAD_ROWS 4
 #define KEYPAD_COLS 4
 
-// CLK
-#define SKPIN_CLK 6
-// SH/~LD
-#define SKPIN_LD 7
-// Q_H
-#define SKPIN_DATA 5
-// CLK INH
-#define SKPIN_CKE 4
-#include <SerialKeypad.h>
+#define PIN_SCK 13
+#define PIN_MISO 12
+#define PIN_MOSI 11
+#define PIN_SS 10
+#define PIN_SSIN 9
+#define PIN_LOAD 8
+#define PIN_INTERRUPT 3
 
 #define DEBUG
 
-static LM35 temp(1100);
+#include <SPI.h>
+
+LM35 temp(1100);
 
 const unsigned long startTime = millis();
 
@@ -42,7 +42,6 @@ const unsigned long startTime = millis();
 
 // Keypad kp = Keypad((char*)hexaKeys, rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
 
-SerialKeypad skp(SKPIN_CLK, SKPIN_LD, SKPIN_CKE, SKPIN_DATA);
 SSD1306AsciiAvrI2c oled;
 
 // TODO 1. use state machine to drive rendering
@@ -62,17 +61,42 @@ SSD1306AsciiAvrI2c oled;
  * *=display on/off (display turns off after 5 second delay)
  */
 
-TempAlarmControl* control;
+// TempAlarmControl* control;
+
+volatile bool keyDown = false;
 
 void setup() {
     analogReference(EXTERNAL);
-    Serial.begin(9600);
-    Serial.println("started setup");
+    Serial.begin(115200);
+    Serial.println(F("started setup"));
     oled.begin(&Adafruit128x64, I2C_ADDRESS);
     delay(2000);
     oled.set2X();
+    Serial.println(F("oled initalized"));
+    
+    // control = new TempAlarmControl();
+    // Serial.println(F("control initialized"));
+    
+    pinMode(PIN_SS, OUTPUT); // Used to enable 74HC595
+    digitalWrite(PIN_SS, HIGH); // init diabled
 
-    control = new TempAlarmControl();
+    pinMode(PIN_SSIN, OUTPUT); // Used to enable 74HC165 clock
+    digitalWrite(PIN_SSIN, HIGH); // init disabled
+
+    pinMode(PIN_LOAD, OUTPUT); // Used to transfer keypad data into 74HC165
+    digitalWrite(PIN_LOAD, HIGH); // init disabled
+    Serial.println(F("pins initialized"));
+    
+    SPI.begin();
+
+    Serial.println(F("spi initalized"));
+    pinMode(PIN_INTERRUPT, INPUT);
+    
+    attachInterrupt(1, keyPressed, HIGH);
+    Serial.println(F("interrupt initalized"));
+    write595(0b11111111);
+    Serial.println(F("595 initalized"));
+    Serial.println(F("Setup done!"));
 }
 
 unsigned long lastPrintTime = 0;
@@ -87,27 +111,100 @@ unsigned long elapsed() {
 
 #define LEFT_PADDING 3
 
-bool keydown = false;
-// char currentKey = NULL;
-byte currentKey = NULL;
+// byte currentKey = NULL;
 unsigned long keydownTime = 0;
 #define KEYDOWN_DECAY 250
+
+// interrupt callback
+void keyPressed() {
+    keyDown = true;
+}
+
+uint8_t processKey() {
+    static uint8_t lastKey;
+    uint8_t currentKey = readKey();
+
+    if (currentKey) {
+        lastKey = currentKey;
+    } else {
+        lastKey = 0;
+    }
+    return currentKey;
+}
+
+uint8_t readKey() {
+    // TODO
+    uint8_t z = 0;
+
+    write595(0b11111111);
+    z = read165();
+    if (z == 0) {
+        return 0;
+    }
+
+    uint8_t key = 1;
+    for (uint8_t i = 1; i <= 0b1000; i <<= 1) {
+        write595(i);
+        z = read165();
+
+        if (z != 0) {
+            break;
+        }
+        key += 4;
+    }
+
+    if (z == 0) {
+        return 0;
+    }
+
+    for (uint8_t col = 0; col < 4; col++) {
+        if ((1<<col) == z) {
+            return key + col;
+        }
+    }
+
+    return 0;
+}
+
+void write595(uint8_t b) {
+    digitalWrite(PIN_SS, LOW); // enable 595
+    asm("nop\n nop\n"); // short delay
+    SPI.transfer(b); // send byte
+    digitalWrite(PIN_SS, HIGH); // disable 595
+}
+
+uint8_t read165() {
+    digitalWrite(PIN_LOAD, LOW); // pulse load
+    asm("nop\n nop\n");
+    digitalWrite(PIN_LOAD, HIGH);
+    digitalWrite(PIN_SSIN, LOW); // enable 165 clk
+    asm("nop\n nop\n");
+    uint8_t b = SPI.transfer(0);
+    digitalWrite(PIN_SSIN, HIGH);
+    return b;
+}
 
 void loop() {
     oled.setFont(TEMP_FONT);
     uint8_t rows = oled.fontRows();
     // currentKey = kp.getKey();
-    currentKey = skp.getKey();
-    if (currentKey) {
-        keydown = true;
-        keydownTime = elapsed();
-        oled.setFont(ALPHA_FONT);
-        Serial.print("KeyCode: 0x");
-        Serial.println(currentKey, HEX);
-        oled.setCursor(LEFT_PADDING, rows);
-        oled.print(currentKey, HEX);
-    } else if (keydown && (elapsed() - keydownTime) > KEYDOWN_DECAY) {
-        keydown = false;
+    if (keyDown) {
+        uint8_t currentKey = processKey();
+        keyDown = false;
+        if (currentKey) {
+            Serial.print(F("Key: "));
+            Serial.print(currentKey, HEX);
+            Serial.print(F(" ("));
+            Serial.print(currentKey, BIN);
+            Serial.println(F(")"));
+            keydownTime = elapsed();
+            oled.setFont(ALPHA_FONT);
+            Serial.print(F("KeyCode: 0x"));
+            Serial.println(currentKey, HEX);
+            oled.setCursor(LEFT_PADDING, rows);
+            oled.print(currentKey, HEX);
+        }
+    } else if (elapsed() - keydownTime > KEYDOWN_DECAY) {
         oled.clear(0,128,rows,2*rows);
     }
 
@@ -121,7 +218,7 @@ void loop() {
     uint8_t mag = (uint8_t)log10(abs(t));
     dtostrf(t, 3 + mag, 1, cstemp);
 
-    Serial.print("update display temp: ");
+    Serial.print(F("update display temp: "));
     Serial.println(cstemp);
     if (mag != lastMag) {
         oled.clear();
